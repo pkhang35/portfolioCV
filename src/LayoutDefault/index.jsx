5c27ff0838afe28db8fee2c5ed940769774c21b0
pkhang35 <pkhang353@gmail.com>
2026-08-29 00:23:19 +0700

    working dark mode basic

--- src/LayoutDefault/index.jsx
+++ src/LayoutDefault/index.jsx
@@ -1,14 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 
-function LayoutDefault (){
-    return(
-        <>
-        <Header/>
-            <main>
-                <Outlet/>
-            </main>
-        </>
-    )
+
+function LayoutDefault() {
+  return (
+    <div className="min-h-screen bg-white text-black transition-colors duration-300 dark:bg-dark-mode dark:text-white">
+      <Header />
+
+      <Outlet />
+
+    </div>
+  );
 }
+
 export default LayoutDefault;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,25 +1,82 @@
-import {ChartColumnIncreasing, Folder, GraduationCap, House, Languages, Moon, WrenchOff} from 'lucide-react';
-function Header(){
-    return(
-        <>
-            <header className=''>
-                <div className='container mx-auto'>
-                    <div className='flex items-center justify-center'>
-                        <ul className='mt-3 p-2 bg-amber-200 rounded-2xl flex gap-3 items-center justify-center'>
-                            <li><House/></li>
-                            <li><WrenchOff /></li>
-                            <li><Folder /></li>
-                            <li><ChartColumnIncreasing /></li>
-                            <li><GraduationCap /></li>
-                            <li><Languages /></li>
-                            <li>
-                                <button className='flex items-center'><Moon/></button>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </header>
-        </>
-    )
+import {
+  ChartColumnIncreasing,
+  Folder,
+  GraduationCap,
+  House,
+  Languages,
+  Moon,
+  Sun,
+  WrenchOff,
+} from "lucide-react";
+import { useState } from "react";
+function Header() {
+  const [isDark, setIsDark] = useState(false);
+  const handleToggleTheme = () => {
+    setIsDark(!isDark);
+     document.documentElement.classList.toggle("dark");
+  };
+  return (
+    <>
+      <header>
+        <div className="container mx-auto">
+          <div className="h-15 flex items-center justify-center">
+            <ul className="mt-3 p-2 bg-gray-light dark:bg-gray-dark rounded-2xl flex gap-3 items-center justify-center">
+              <li>
+                <House />
+              </li>
+              <li>
+                <WrenchOff />
+              </li>
+              <li>
+                <Folder />
+              </li>
+              <li>
+                <ChartColumnIncreasing />
+              </li>
+              <li>
+                <GraduationCap />
+              </li>
+              <li>
+                <Languages />
+              </li>
+              <li>
+                <button
+                  onClick={handleToggleTheme}
+                  type="button"
+                  className={`
+                        relative 
+                        flex 
+                        h-9 w-18 
+                        items-center 
+                        rounded-full 
+                        border border-gray-300
+                        p-1 
+                        ${isDark?"bg-toggle-dark ":"bg-toggle-light"}    
+                    `}
+                >
+                  <span
+                    className={`
+                        flex items-center justify-center
+                        size-7
+                        transition-all
+                        duration-300
+                        rounded-full 
+                        ${isDark ? "translate-x-9 bg-dark-mode" : "translate-x-0 bg-gray-light "}
+                    `}
+                  >
+                    {isDark ? (
+                      <Moon className="size-5 text-white" />
+                    ) : (
+                      <Sun className="size-5 text-yellow-500" />
+                    )}
+                  </span>
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </header>
+    </>
+  );
 }
 export default Header;
