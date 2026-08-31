025071265ec906bc83d784d89e7db30a05a2fe43
pkhang35 <pkhang353@gmail.com>
2026-08-31 20:04:53 +0700

    add section hero

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,25 +2,23 @@ import Github from "../../assets/icons/github-color-svgrepo-com.svg";
 import Linkedin from "../../assets/icons/linkedin-svgrepo-com.svg";
 import Mail from "../../assets/icons/new-logo-gmail-svgrepo-com.svg";
 import Zalo from "../../assets/icons/zalo-svgrepo-com.svg";
+import Git from "../../assets/icons/git-svgrepo-com.svg";
+import React from "../../assets/icons/react-javascript-js-framework-facebook-svgrepo-com.svg";
+import JS from "../../assets/icons/js-svgrepo-com.svg";
+import Tailwind from "../../assets/icons/tailwind-svgrepo-com.svg";
 
+import {FileUser,ArrowRight} from "lucide-react";
 function Home() {
   return (
     <>
       {/* Hero */}
       <section className="py-20">
         <div className="container mx-auto">
-
           <div className="flex items-center justify-between">
-
-            {/* ================= LEFT ================= */}
             <div className="w-1/2">
-
-              {/* Greeting */}
               <p className="mb-4 text-lg font-medium text-primary">
                 Xin chào, mình là Khang 👋
               </p>
-
-              {/* Heading */}
               <h1 className="font-heading text-6xl leading-tight">
                 Fresher{" "}
                 <span className="text-primary">
@@ -28,21 +26,21 @@ function Home() {
                 </span>{" "}
                 Developer
               </h1>
-
-              {/* Description */}
               <p className="mt-6 max-w-xl text-lg leading-8 text-gray-600 dark:text-gray-300">
                 Mình xây dựng các website hiện đại,
                 responsive và tập trung vào trải nghiệm
                 người dùng.
               </p>
-
-              {/* Buttons */}
               <div className="mt-8 flex items-center gap-4">
-
                 <button
                   type="button"
                   className="
+                    flex
+                    items-center
+                    gap-2
                     rounded-lg
+                    border
+                    border-primary
                     bg-primary
                     px-6
                     py-3
@@ -54,12 +52,15 @@ function Home() {
                     hover:shadow-lg
                   "
                 >
-                  Xem dự án →
+                  Xem dự án <ArrowRight className="size-5" />
                 </button>
 
                 <button
                   type="button"
                   className="
+                    flex
+                    items-center
+                    gap-2
                     rounded-lg
                     border
                     border-primary
@@ -72,23 +73,17 @@ function Home() {
                     hover:-translate-y-1
                     hover:bg-primary
                     hover:text-white
-                  "
-                >
-                  Liên hệ với mình
+                  ">
+                  Xem CV của mình <FileUser className="size-5"/>
                 </button>
-
               </div>
 
-              {/* Social */}
               <div className="mt-10">
-
                 <p className="mb-4 font-medium">
                   Kết nối với mình
                 </p>
 
                 <ul className="flex items-center gap-5">
-
-                  {/* Github */}
                   <li>
                     <a
                       href="https://github.com/pkhang35"
@@ -110,7 +105,6 @@ function Home() {
                     </a>
                   </li>
 
-                  {/* Linkedin */}
                   <li>
                     <a
                       href="https://www.linkedin.com/in/pkhang35/"
@@ -132,7 +126,6 @@ function Home() {
                     </a>
                   </li>
 
-                  {/* Email */}
                   <li>
                     <a
                       href="mailto:pkhang353@gmail.com"
@@ -152,7 +145,6 @@ function Home() {
                     </a>
                   </li>
 
-                  {/* Zalo */}
                   <li>
                     <a
                       href="https://zalo.me/0332172749"
@@ -164,8 +156,7 @@ function Home() {
                         src={Zalo}
                         alt="Zalo"
                         className="
-                          h-10
-                          w-10
+                          size-10
                           transition-transform
                           duration-200
                           hover:scale-110
@@ -173,71 +164,25 @@ function Home() {
                       />
                     </a>
                   </li>
-
                 </ul>
-
               </div>
-
             </div>
 
-
-            {/* ================= RIGHT ================= */}
             <div className="relative flex w-1/2 justify-end">
-
-              {/* Code Window */}
-              <div
-                className="
-                  w-full
-                  max-w-xl
-                  overflow-hidden
-                  rounded-2xl
-                  bg-gray-dark
-                  shadow-2xl
-                "
-              >
-
-                {/* Window Header */}
-                <div
-                  className="
-                    flex
-                    h-14
-                    items-center
-                    border-b
-                    border-gray-700
-                    px-5
-                  "
-                >
-
-                  {/* Window buttons */}
+              <div className=" w-full max-w-xl overflow-hidden rounded-2xl bg-gray-dark shadow-2xl">
+                <div className="flex h-14 items-center border-b border-gray-700 px-5">
                   <div className="flex gap-2">
-
                     <span className="size-3 rounded-full bg-red-400"></span>
-
                     <span className="size-3 rounded-full bg-yellow-400"></span>
-
                     <span className="size-3 rounded-full bg-green-400"></span>
-
                   </div>
-
-                  {/* File name */}
                   <span className="ml-5 font-mono text-sm text-gray-300">
                     Khang.js
                   </span>
-
                 </div>
 
-
-                {/* Code Content */}
                 <div className="p-7">
-
-                  <pre
-                    className="
-                      overflow-hidden
-                      font-mono
-                      text-sm
-                      leading-7
-                    "
-                  >
+                  <pre className=" overflow-hidden font-mono text-sm leading-7">
                     <code>
 
                       <span className="text-primary">
@@ -381,97 +326,26 @@ function Home() {
                       <span className="text-gray-300">
                         {"}"}
                       </span>
-
                     </code>
                   </pre>
-
                 </div>
-
               </div>
 
+              <div className="absolute -bottom-10 left-1/2 flex -translate-x-1/2 items-center gap-5">
 
-              {/* ================= FLOATING ICONS ================= */}
-
-              <div
-                className="
-                  absolute
-                  -bottom-10
-                  left-1/2
-                  flex
-                  -translate-x-1/2
-                  items-center
-                  gap-5
-                "
-              >
-
-                {/* React */}
-                <div
-                  className="
-                    flex
-                    size-20
-                    items-center
-                    justify-center
-                    rounded-full
-                    bg-white
-                    text-3xl
-                    shadow-xl
-                  "
-                >
-                  ⚛️
+                <div className="flex size-20 items-center justify-center rounded-full bg-white text-3xl shadow-xl">
+                  <img src={React} alt="react" className=" size-10"/>
+                </div>
+                <div className="flex size-20 items-center justify-center rounded-full bg-white shadow-xl">
+                  <img src={JS} alt="js" className=" size-10"/>
                 </div>
 
-
-                {/* JavaScript */}
-                <div
-                  className="
-                    flex
-                    size-20
-                    items-center
-                    justify-center
-                    rounded-full
-                    bg-white
-                    shadow-xl
-                  "
-                >
-                  <span className="font-bold text-yellow-500">
-                    JS
-                  </span>
+                <div className="flex size-20 items-center justify-center rounded-full bg-white shadow-xl">
+                  <img src={Tailwind} alt="tailwind css" className=" size-10"/>
                 </div>
 
-
-                {/* Tailwind */}
-                <div
-                  className="
-                    flex
-                    size-20
-                    items-center
-                    justify-center
-                    rounded-full
-                    bg-white
-                    shadow-xl
-                  "
-                >
-                  <span className="text-3xl font-bold text-cyan-500">
-                    ~
-                  </span>
-                </div>
-
-
-                {/* Git */}
-                <div
-                  className="
-                    flex
-                    size-20
-                    items-center
-                    justify-center
-                    rounded-full
-                    bg-white
-                    shadow-xl
-                  "
-                >
-                  <span className="font-bold text-purple-500">
-                    Git
-                  </span>
+                <div className="flex size-20 items-center justify-center rounded-full bg-white shadow-xl">
+                 <img src={Git} alt="git" className=" size-10"/>
                 </div>
 
               </div>
